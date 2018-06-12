4aabc966d8824b9f48e9a27d35c7d4db417b31d0
fengyuexingzi <fengyuexingzi@gmail.com>
2018-06-12 16:11:34 +0800

    auth

--- auth/linkedin/index.php
+++ auth/linkedin/index.php
@@ -6,4 +6,26 @@
  * Time: 15:17
  */
 
-print_r($GLOBALS);
+// https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=81cc4t9fuu9bpu&redirect_uri=https%3A%2F%2Ffengyuexingzi.top%2Fauth%2Flinkedin&state=987654321&scope=r_basicprofile
+
+$url = "https://www.linkedin.com/oauth/v2/accessToken";
+
+$data = array(
+    'grant_type' => 'authorization_code',
+    'code' => $_GET['code'],
+    'redirect_uri' => 'https://fengyuexingzi.top/auth/linkedin',
+    'client_id' => '81cc4t9fuu9bpu',
+    'client_secret' => 'GdY3ZO8LAHn8y4tX',
+);
+
+$ch = init($url);
+curl_setopt($ch,CURLOPT_POST,true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, build_query($data));
+curl_setopt($ch, CURLOPT_TRANSFERTEXT, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_HEADER, false);
+$result = curl_exec($ch);
+$info = curl_getinfo($ch);
+curl_close($ch);
+print_r($info);
+var_dump($result);
