c02ebafa5b078e1a960209ef7a10599444bba4ae
fengyuexingzi <fengyuexingzi@gmail.com>
2018-06-12 16:13:48 +0800

    auth

--- auth/linkedin/index.php
+++ auth/linkedin/index.php
@@ -8,6 +8,12 @@
 
 // https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=81cc4t9fuu9bpu&redirect_uri=https%3A%2F%2Ffengyuexingzi.top%2Fauth%2Flinkedin&state=987654321&scope=r_basicprofile
 
+register_shutdown_function('shutdown');
+
+function shutdown(){
+    error_get_last();
+}
+
 $url = "https://www.linkedin.com/oauth/v2/accessToken";
 
 $data = array(
