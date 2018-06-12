f5ed86421a619f7c23a17d1eb4c562ad507c8a34
fengyuexingzi <fengyuexingzi@gmail.com>
2018-06-12 17:26:07 +0800

    auth

--- auth/linkedin/index.php
+++ auth/linkedin/index.php
@@ -33,6 +33,8 @@ function curl_get($url, $data)
     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
     curl_setopt($ch, CURLOPT_HEADER, false);
     $result = curl_exec($ch);
+    $info = curl_getinfo($ch);
+    $result = array_merge($result, $info);
     curl_close($ch);
     return json_decode($result, true);
 }
